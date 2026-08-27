7f3f4a8cd1595fce820be2de01fc49786c35acbe
angpe995 <90847653+angpe995@users.noreply.github.com>
2026-08-28 00:36:02 +0200

    fix: implement UpdatePrice() method for Room model

--- ConferenceRoomBookingApi/Models/Room.cs
+++ ConferenceRoomBookingApi/Models/Room.cs
@@ -1,9 +1,19 @@
 namespace ConferenceRoomBookingApi.Models;
+
 public class Room
 {
-    public int Id{get;set;}
-    public string Name{get;set;}=string.Empty;
-    public decimal PricePerHour{get; set;}
-    public int Capacity{get; set;}
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal PricePerHour { get; private set; }
+    public int Capacity { get; set; }
     public List<Service> Services { get; set; } = new();
+    public void UpdatePrice(decimal newPrice)
+    {
+        if (newPrice <= 0)
+        {
+            return;
+        }
+
+        PricePerHour = newPrice;
+    }
 }
